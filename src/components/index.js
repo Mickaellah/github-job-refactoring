658fe0061ea456072a71df34d755cb4237328a0d
Nomenjanahary Clopedia <clopedia.nom@onja.org>
2021-01-28 16:18:26 +0300

    Created folders in the components folder and added some files in those folders, added a new file named index.js in the components folder for exporting all of the components.

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export {default as Header} from './header';
+export {default as SearchForm} from './search-form';
+export {default as Cards} from './cards';
+export {default as JobCheckbox} from './job_checkbox';
+export {default as LocationCheckboxes} from './location_checkboxes';
+export {default as LocationSearchForm} from './location_search_form';
