df73a4d4c448293f37f787c77750e8027cedf8ae
Nomenjanahary Clopedia <clopedia.nom@onja.org>
2021-01-28 17:27:49 +0300

    Added a file in the pages folder.

---
+++ src/components/header/styles/header.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+    padding: 16px;
+`;
+
+export const Text = styled.h1`
+    font-style: 36px;
+    font-weight: bold;
+    text-transform: uppercase;
+`;
---
+++ src/containers/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components';
+
+export default function HeaderContainer() {
+    return (
+        <Header>
+            {/* <Header.Container> */}
+                <Header.Text>
+                    Gigthub job
+                </Header.Text>
+            {/* </Header.Container> */}
+        </Header>
+    )
+}
