3ad946528618031a0f06fa4f5d7ca962f8ff3f1f
Nomenjanahary Clopedia <clopedia.nom@onja.org>
2021-01-30 14:11:45 +0300

    Added functionality to the search form input to filter jobs by its title.

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -37273,60 +37273,6 @@ function HeaderContainer(_ref) {
   var children = _ref.children;
   return /*#__PURE__*/_react.default.createElement(_components.Header, null, /*#__PURE__*/_react.default.createElement(_components.Header.Text, null, "Gigthub job"), children);
 }
-},{"react":"node_modules/react/index.js","../components":"src/components/index.js"}],"src/containers/search_form.js":[function(require,module,exports) {
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = SearchFormContainer;
-
-var _react = _interopRequireDefault(require("react"));
-
-var _components = require("../components");
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function SearchFormContainer(_ref) {
-  var children = _ref.children;
-  return /*#__PURE__*/_react.default.createElement(_components.SearchForm, null, /*#__PURE__*/_react.default.createElement(_components.SearchForm.Input, {
-    type: "search",
-    placeholder: "Title, companies, expertise"
-  }), /*#__PURE__*/_react.default.createElement(_components.SearchForm.Button, {
-    type: "submit"
-  }, "Search"), children);
-}
-},{"react":"node_modules/react/index.js","../components":"src/components/index.js"}],"src/containers/checkboxes.js":[function(require,module,exports) {
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = CheckboxesContainer;
-
-var _react = _interopRequireDefault(require("react"));
-
-var _components = require("../components");
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function CheckboxesContainer(_ref) {
-  var children = _ref.children;
-  return /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement(_components.JobCheckbox, null, /*#__PURE__*/_react.default.createElement(_components.JobCheckbox.Input, {
-    type: "checkbox"
-  }), /*#__PURE__*/_react.default.createElement(_components.JobCheckbox.Label, null, "Full time")), /*#__PURE__*/_react.default.createElement(_components.LocationSearchForm, null, /*#__PURE__*/_react.default.createElement(_components.LocationSearchForm.Input, {
-    type: "text",
-    placeholder: "City, state, zip code or country"
-  }), /*#__PURE__*/_react.default.createElement(_components.LocationSearchForm.Label, null, "Location")), /*#__PURE__*/_react.default.createElement(_components.LocationCheckboxes, null, /*#__PURE__*/_react.default.createElement(_components.LocationCheckboxes.Fieldset, null, /*#__PURE__*/_react.default.createElement(_components.LocationCheckboxes.Input, {
-    type: "checkbox"
-  }), /*#__PURE__*/_react.default.createElement(_components.LocationCheckboxes.Label, null, "London")), /*#__PURE__*/_react.default.createElement(_components.LocationCheckboxes.Fieldset, null, /*#__PURE__*/_react.default.createElement(_components.LocationCheckboxes.Input, {
-    type: "checkbox"
-  }), /*#__PURE__*/_react.default.createElement(_components.LocationCheckboxes.Label, null, "Amsterdam")), /*#__PURE__*/_react.default.createElement(_components.LocationCheckboxes.Fieldset, null, /*#__PURE__*/_react.default.createElement(_components.LocationCheckboxes.Input, {
-    type: "checkbox"
-  }), /*#__PURE__*/_react.default.createElement(_components.LocationCheckboxes.Label, null, "New York")), /*#__PURE__*/_react.default.createElement(_components.LocationCheckboxes.Fieldset, null, /*#__PURE__*/_react.default.createElement(_components.LocationCheckboxes.Input, {
-    type: "checkbox"
-  }), /*#__PURE__*/_react.default.createElement(_components.LocationCheckboxes.Label, null, "Berlin"))));
-}
 },{"react":"node_modules/react/index.js","../components":"src/components/index.js"}],"src/context/context.js":[function(require,module,exports) {
 "use strict";
 
@@ -37425,7 +37371,107 @@ function ContextProvider(_ref) {
     }
   }, children);
 }
-},{"react":"node_modules/react/index.js"}],"src/containers/card.js":[function(require,module,exports) {
+},{"react":"node_modules/react/index.js"}],"src/containers/search_form.js":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = SearchFormContainer;
+
+var _react = _interopRequireWildcard(require("react"));
+
+var _context = require("../context/context");
+
+var _components = require("../components");
+
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function () { return cache; }; return cache; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+function SearchFormContainer(_ref) {
+  var children = _ref.children;
+
+  var _useState = (0, _react.useState)(''),
+      _useState2 = _slicedToArray(_useState, 2),
+      jobTitle = _useState2[0],
+      setJobTitle = _useState2[1];
+
+  var _useContext = (0, _react.useContext)(_context.Context),
+      state = _useContext.state,
+      dispatch = _useContext.dispatch;
+
+  var data = state.data;
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    var jobs = data.filter(function (job) {
+      if (!jobTitle) return undefined;
+      return job.title.toLowerCase().includes(jobTitle.toLowerCase());
+    });
+    dispatch({
+      type: "JOBS",
+      job: jobs
+    });
+  }
+
+  return /*#__PURE__*/_react.default.createElement(_components.SearchForm, {
+    onSubmit: handleSubmit
+  }, /*#__PURE__*/_react.default.createElement(_components.SearchForm.Input, {
+    type: "search",
+    value: jobTitle,
+    onChange: function onChange(e) {
+      return setJobTitle(e.target.value);
+    },
+    placeholder: "Title, companies, expertise"
+  }), /*#__PURE__*/_react.default.createElement(_components.SearchForm.Button, {
+    type: "submit"
+  }, "Search"), children);
+}
+},{"react":"node_modules/react/index.js","../context/context":"src/context/context.js","../components":"src/components/index.js"}],"src/containers/checkboxes.js":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = CheckboxesContainer;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _components = require("../components");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function CheckboxesContainer(_ref) {
+  var children = _ref.children;
+  return /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement(_components.JobCheckbox, null, /*#__PURE__*/_react.default.createElement(_components.JobCheckbox.Input, {
+    type: "checkbox"
+  }), /*#__PURE__*/_react.default.createElement(_components.JobCheckbox.Label, null, "Full time")), /*#__PURE__*/_react.default.createElement(_components.LocationSearchForm, null, /*#__PURE__*/_react.default.createElement(_components.LocationSearchForm.Input, {
+    type: "text",
+    placeholder: "City, state, zip code or country"
+  }), /*#__PURE__*/_react.default.createElement(_components.LocationSearchForm.Label, null, "Location")), /*#__PURE__*/_react.default.createElement(_components.LocationCheckboxes, null, /*#__PURE__*/_react.default.createElement(_components.LocationCheckboxes.Fieldset, null, /*#__PURE__*/_react.default.createElement(_components.LocationCheckboxes.Input, {
+    type: "checkbox"
+  }), /*#__PURE__*/_react.default.createElement(_components.LocationCheckboxes.Label, null, "London")), /*#__PURE__*/_react.default.createElement(_components.LocationCheckboxes.Fieldset, null, /*#__PURE__*/_react.default.createElement(_components.LocationCheckboxes.Input, {
+    type: "checkbox"
+  }), /*#__PURE__*/_react.default.createElement(_components.LocationCheckboxes.Label, null, "Amsterdam")), /*#__PURE__*/_react.default.createElement(_components.LocationCheckboxes.Fieldset, null, /*#__PURE__*/_react.default.createElement(_components.LocationCheckboxes.Input, {
+    type: "checkbox"
+  }), /*#__PURE__*/_react.default.createElement(_components.LocationCheckboxes.Label, null, "New York")), /*#__PURE__*/_react.default.createElement(_components.LocationCheckboxes.Fieldset, null, /*#__PURE__*/_react.default.createElement(_components.LocationCheckboxes.Input, {
+    type: "checkbox"
+  }), /*#__PURE__*/_react.default.createElement(_components.LocationCheckboxes.Label, null, "Berlin"))));
+}
+},{"react":"node_modules/react/index.js","../components":"src/components/index.js"}],"src/containers/card.js":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -37695,7 +37741,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59091" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65486" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/containers/search_form.js
+++ src/containers/search_form.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, {useState, useContext} from 'react';
 
+import {Context} from '../context/context';
 import {SearchForm} from '../components';
 
 export default function SearchFormContainer({children}) {
+    const [jobTitle, setJobTitle] = useState('');
+    const {state, dispatch} = useContext(Context);
+    const {data} = state;
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        const jobs = data.filter(job => { 
+            if (!jobTitle) return undefined;
+            return job.title.toLowerCase().includes(jobTitle.toLowerCase());
+        });
+
+        dispatch({type: "JOBS", job: jobs});
+    }
+
     return (
-        <SearchForm>
-            <SearchForm.Input type="search" placeholder="Title, companies, expertise" />
+        <SearchForm onSubmit={handleSubmit}>
+            <SearchForm.Input type="search" value={jobTitle} onChange={(e) => setJobTitle(e.target.value)} placeholder="Title, companies, expertise" />
             <SearchForm.Button type="submit">Search</SearchForm.Button>
             {children}
         </SearchForm>
